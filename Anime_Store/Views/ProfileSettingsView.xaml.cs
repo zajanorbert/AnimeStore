5459dc1e13c2e3d532b04610f15b6d056138ea68
YungShagger <pridak7@gmail.com>
2019-12-05 16:10:43 +0100

    Profile settings view hase been updated. Adjustments to Acoount window has been made.

--- Anime_Store/Views/ProfileSettingsView.xaml.cs
+++ Anime_Store/Views/ProfileSettingsView.xaml.cs
@@ -24,5 +24,43 @@ namespace Anime_Store.Views
         {
             InitializeComponent();
         }
+        //Image Events
+        private void ImageChangerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //Description Events
+        private void ChangeDescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            DescriptionTextBlock.Visibility = Visibility.Hidden;
+            DescriptionTextBox.Visibility = Visibility.Visible;
+            SaveDescriptionButton.Visibility = Visibility.Visible;
+            BackFromDescriptionButton.Visibility = Visibility.Visible;
+            ChangeDescriptionButton.Visibility = Visibility.Hidden;
+
+        }
+
+        private void SaveDescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            DescriptionTextBlock.Visibility = Visibility.Visible;
+            DescriptionTextBox.Visibility = Visibility.Hidden;
+            SaveDescriptionButton.Visibility = Visibility.Hidden;
+            BackFromDescriptionButton.Visibility = Visibility.Hidden;
+            ChangeDescriptionButton.Visibility = Visibility.Visible;
+            DescriptionTextBlock.Text = DescriptionTextBox.Text;
+        }
+
+        private void BackFromDescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            DescriptionTextBlock.Visibility = Visibility.Visible;
+            DescriptionTextBox.Visibility = Visibility.Hidden;
+            SaveDescriptionButton.Visibility = Visibility.Hidden;
+            BackFromDescriptionButton.Visibility = Visibility.Hidden;
+            ChangeDescriptionButton.Visibility = Visibility.Visible;
+
+        }
+
+
+
     }
 }
