bd76b4b698e90364aceb2bd7a567401e50b497bf
YungShagger <pridak7@gmail.com>
2020-01-17 11:22:26 +0100

    Media player has been added and functioning. the seek slider is still laggy.

--- Anime_Store/Views/VideoPlayerView.xaml.cs
+++ Anime_Store/Views/VideoPlayerView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Anime_Store.Views
 {
@@ -20,9 +21,74 @@ namespace Anime_Store.Views
     /// </summary>
     public partial class VideoPlayerView : UserControl
     {
+        DispatcherTimer timer;
         public VideoPlayerView()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            SeekSlider.Value = MediaElement1.Position.TotalSeconds;
+        }
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.DataContext = new AnimeInfoView();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Play();
+            PlayButton.Visibility = Visibility.Hidden;
+            PauseButton.Visibility = Visibility.Visible;
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Pause();
+            PauseButton.Visibility = Visibility.Hidden;
+            PlayButton.Visibility = Visibility.Visible;
+
+        }
+
+        private void StopButtun_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Stop();
+            SeekSlider.Value = 0;
+            PauseButton.Visibility = Visibility.Hidden;
+            PlayButton.Visibility = Visibility.Visible;
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MediaElement1.Volume = (double)VolumeSlider.Value;
+        }
+
+        private void SeekSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MediaElement1.Position = TimeSpan.FromSeconds(SeekSlider.Value);
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            string filename = (string)((DataObject)e.Data).GetFileDropList()[0];
+            MediaElement1.Source = new Uri(filename);
+            MediaElement1.LoadedBehavior = MediaState.Manual;
+            MediaElement1.UnloadedBehavior = MediaState.Manual;
+            MediaElement1.Volume = (double)VolumeSlider.Value;
+            MediaElement1.Play();
+        }
+
+        private void MediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            TimeSpan ts = MediaElement1.NaturalDuration.TimeSpan;
+            SeekSlider.Maximum = ts.TotalSeconds;
+            timer.Start();
+        }
+
     }
 }
